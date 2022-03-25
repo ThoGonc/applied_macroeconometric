bc8e217a965992bb678b06131911c3e6d7b3b055
ThoGonc <95226254+ThoGonc@users.noreply.github.com>
2022-03-25 22:29:41 +0100

    Maj_HP

--- code.R
+++ code.R
@@ -1,4 +1,16 @@
-install.packages("readxl")
+#install.packages("readxl")
+install.packages("mfilter")
 library(RCurl)
+library(mFilter)
+
 urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_applied.csv'
 dsin<-read.csv2(urlfile, header=TRUE)
+myts<-ts(data=dsin,start=(1970),end=(2022),frequency=4)
+France<-dsin[[5]]
+
+
+Francets<-ts(data=France,start=(1970),end=(2022),frequency=4)
+
+
+France_hp<- hpfilter(Francets, freq=1600,type="frequency",drift=TRUE)
+
