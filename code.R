df9722b864f6bde6a2e2a5eafa085fa459cc1673
ThoGonc <95226254+ThoGonc@users.noreply.github.com>
2022-03-31 16:47:28 +0200

    maj

--- code.R
+++ code.R
@@ -3,13 +3,13 @@ install.packages("mfilter")
 library(RCurl)
 library(mFilter)
 
-urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_applied.csv'
+urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_applied_pib_volume.csv'
 dsin<-read.csv2(urlfile, header=TRUE)
 myts<-ts(data=dsin,start=(1970),end=(2022),frequency=4)
-France<-dsin[[5]]
+France<-dsin[[6]]
 
 
-Francets<-ts(data=France,start=(1970),end=(2022),frequency=4)
+Francets<-ts(data=France,start=(1970),end=(2021),frequency=4)
 
 
 France_hp<- hpfilter(Francets, freq=1600,type="frequency",drift=TRUE)
