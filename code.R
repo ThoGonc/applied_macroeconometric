dfc590aff654509915bc2b4b6249f86a2752b037
ThoGonc <95226254+ThoGonc@users.noreply.github.com>
2022-04-16 10:09:57 +0200

    Code_applied_HP

--- code.R
+++ code.R
@@ -2,6 +2,7 @@
 #install.packages("mfilter")
 library(RCurl)
 library(mFilter)
+library(tidyverse)
 
 urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_applied_pib_volume.csv'
 dsin<-read.csv2(urlfile, header=TRUE)
@@ -14,6 +15,7 @@ Francets<-ts(data=France,start=(1970),end=(2021),frequency=4)
 
 France_hp<- hpfilter(Francets, freq=1600,type="frequency",drift=TRUE)
 
+par(ask=F)
 plot(France_hp)
 
 
@@ -35,7 +37,7 @@ plot(Suede_hp)
 USA<-dsin[[33]]
 
 
-USAts<-ts(data=USA,start=(1970),end=(2021),frequency=4)
+USAts<-ts(data=USA,start=(1970),end=(2031),frequency=4)
 
 
 USA_hp<- hpfilter(USAts, freq=1600,type="frequency",drift=TRUE)
@@ -43,6 +45,16 @@ USA_hp<- hpfilter(USAts, freq=1600,type="frequency",drift=TRUE)
 plot(USA_hp)
 
 
+
+Suisse<-dsin[[10]]
+
+
+Suissets<-ts(data=Suisse,start=(1980),end=(2021),frequency=4)
+
+
+Suisse_hp<- hpfilter(Suissets, freq=1600,type="frequency",drift=TRUE)
+
+plot(Suisse_hp)
 
 
 # Kalman filter procedure
