ca92d818b21415f0e55381f6a8f6c8b6dda27a02
ThoGonc <95226254+ThoGonc@users.noreply.github.com>
2022-04-23 21:20:15 +0200

    maj

--- code.R
+++ code.R
@@ -3,6 +3,7 @@
 library(RCurl)
 library(mFilter)
 library(tidyverse)
+library(ggplot2)
 
 urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_applied_pib_volume.csv'
 dsin<-read.csv2(urlfile, header=TRUE)
@@ -19,15 +20,21 @@ Francets<-ts(data=France,start=(1970),end=(2021),frequency=4)
 
 France_hp<- hpfilter(Francets, freq=100,type="frequency",drift=TRUE)
 plot(France_hp)
+France_bw <- bwfilter(Francets)
+plot(France_bw)
 
+cycle_France<-France_hp$cycle
+trend_France<-France_hp$trend
+francetss<-France_hp$x
 
-
+#plot(France_hp)
+ts.plot(francetss, trend_France, gpars = list(col = c("black", "red")))
 
 
 Suede<-dsin[[7]]
 Suedets<-ts(data=Suede,start=(1970),end=(2021),frequency=4)
 Suede_hp<- hpfilter(Suedets, freq=100,type="frequency",drift=TRUE)
-
+devAskNewPage(ask = FALSE)
 plot(Suede_hp)
 
 
@@ -56,22 +63,6 @@ plot(Suisse_hp)
 
 
 
-
-#Output_gap
-
-
-urlfile<-'https://raw.githubusercontent.com/ThoGonc/applied_macroeconometric/main/Data_IMF_annuelles_PIB_output_gap.csv'
-dsin<-read.csv2(urlfile, header=TRUE)
-myts<-ts(data=dsin,start=(1980),end=(2022),frequency=4)
-
-
-France<-dsin[[14]]
-Francets<-ts(data=France,start=(1980),end=(2022),frequency=4)
-
-France_hp<- hpfilter(Francets, freq=100,type="frequency",drift=TRUE)
-plot(France_hp)
-
-
 rm(list = ls())
 
 
@@ -85,5 +76,4 @@ rm(list = ls())
 # Kalman filter procedure
 
 #install.packages("MARSS")
-install.packages("dlm")
-library(dlm)
+library(MARSS)
--- test-kalman.R
+++ test-kalman.R
@@ -1,9 +1,65 @@
 rm(list = ls())
 graphics.off()
 
-install.packages("Rtools")
+
+
+install.packages("devtools")
+devtools::install_github("KevinKotze/tsm")
 install.packages("dlm")
 
 library(tsm)
 library(dlm)
-library(installr) 
+
+
+
+dat <- sarb_quarter$KBP6006L/sarb_quarter$KBP6006D
+dat.tmp <- diff(log(na.omit(dat)) * 100, lag = 1)
+head(dat)
+
+inf <- ts(dat.tmp, start = c(1960, 2), frequency = 4)
+
+plot.ts(inf)
+
+
+fn <- function(parm) {
+  dlmModPoly(order = 1, dV = exp(parm[1]), dW = exp(parm[2]))
+}
+
+
+fit <- dlmMLE(inf, rep(0, 2), build = fn, hessian = TRUE)
+(conv <- fit$convergence)
+
+loglik <- dlmLL(inf, dlmModPoly(1))
+n.coef <- 2
+r.aic <- (2 * (loglik)) + 2 * (sum(n.coef))  #dlmLL caculates the neg. LL
+r.bic <- (2 * (loglik)) + (log(length(inf))) * (n.coef)
+
+
+mod <- fn(fit$par)
+obs.error.var <- V(mod)
+state.error.var <- W(mod)
+
+
+filtered <- dlmFilter(inf, mod = mod)
+smoothed <- dlmSmooth(filtered)
+
+
+resids <- residuals(filtered, sd = FALSE)
+mu <- dropFirst(smoothed$s)
+mu.1 <- mu[1]
+mu.end <- mu[length(mu)]
+
+
+
+
+par(mfrow = c(2, 1), mar = c(2.2, 2.2, 1, 1), cex = 0.8)
+plot.ts(inf, col = "darkgrey", xlab = "", ylab = "", lwd = 1.5)
+lines(mu, col = "black")
+legend("topright", legend = c("Observed Deflator", "Stochastic level"), 
+       lwd = c(2, 1), col = c("darkgrey", "black"), bty = "n")
+
+plot.ts(resids, ylab = "", xlab = "", col = "darkgrey", 
+        lwd = 1.5)
+abline(h = 0)
+legend("topright", legend = "Residuals", lwd = 1.5, col = "darkgrey", 
+       bty = "n")
