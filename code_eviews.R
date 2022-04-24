908cbfa5df70e706e5d658fdd487e9beae87d481
ThoGonc <95226254+ThoGonc@users.noreply.github.com>
2022-04-24 19:38:42 +0200

    test

--- code_eviews.R
+++ code_eviews.R
@@ -3,9 +3,23 @@
 
 library(readxl)
 library(tseries)
+library(mFilter)
 
 
 
 rm(list = ls())
 graphics.off()
 MySheet <- read_excel("D:/2021-22/ENSAE_papiers/Cours/S2/2.1 . Applied Macroeconometrics/applied_macroeconometrics/filtering/pottest.xlsx")
+
+gdp<-MySheet[[3]]
+infl<-MySheet[[5]]
+
+lgdp<-log(gdp)
+dlgdp<-100*diff(lgdp)
+
+
+lgdp_hp<- hpfilter(lgdp, freq=1600,type="frequency",drift=FALSE)    #Vérifier s'il faut un drift ou pas
+lgdpt_hp<-lgdp_hp$trend
+
+
+lgdpc_hp= lgdp - lgdpt_hp
